e5a9072c140b4d2fe1f581672fb505385f06bad4
alisondossantos <classe.metodo@hotmail.com>
2024-02-19 19:41:53 -0300

    exercicio001
    Primeiro algoritmo em python

---
+++ codigos/exercicio0001.py
@@ -0,0 +1,5 @@
+#criar um algoritmo que leia dois numeros e faca a soma
+n1 = int(input('digite o primeiro numero'))
+n2 = int(input('digite o segundo numero'))
+soma = n1 + n2 
+print('a soma vale {}'.format(soma))
